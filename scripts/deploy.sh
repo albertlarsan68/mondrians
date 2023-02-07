f155905bb37eedcb1376ad9a67402ee1df45b4e2
Albert Larsan <74931857+albertlarsan68@users.noreply.github.com>
2023-02-07 14:45:06 +0000

    Update

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+cargo install wasm-pack
+wasm-pack build --target no-modules --mode no-install --no-typescript --release
+mkdir deploy
+HASH=$(git rev-parse --short HEAD)
+mkdir deploy/$HASH
+cp pkg/mondrians.js pkg/mondrians_bg.wasm deploy/$HASH
+cp static/* deploy/$HASH
+mv deploy/$HASH/index.html deploy/
+# Replace {hash} in index.html with the hash of the current commit
+sed -i "s/{hash}/$HASH/g" deploy/index.html
---
+++ static/main.js
@@ -0,0 +1,91 @@
+const form = document.querySelector("form");
+const output = document.querySelector("img#output");
+const submitButton = document.querySelector("input#generate");
+const ramdomFill = document.querySelector("button#randomFill");
+const seed = document.querySelector("input#seed");
+var generating = false;
+var toGenerate = null;
+var erroring = false;
+submitButton.disabled = true;
+submitButton.value = "Chargement en cours...";
+
+var worker = new Worker("worker.js");
+
+function workerOnError(error) {
+    console.log(`Worker error: ${error.message}`);
+    submitButton.disabled = false;
+    submitButton.value = "Erreur lors de la génération, modifiez les paramètres et réessayez";
+    erroring = true;
+    toGenerate = null;
+    generating = false;
+    worker.terminate();
+    setTimeout(() => { worker = new Worker("worker.js"); worker.onerror = workerOnError; worker.onmessage = workerOnMessage; erroring = false; }, 1);
+    throw error;
+}
+
+function workerOnMessage(event) {
+    if (toGenerate != null) {
+        worker.postMessage(toGenerate);
+        toGenerate = null;
+    } else {
+        generating = false;
+        submitButton.disabled = false;
+        submitButton.value = "Générer";
+    }
+    output.src = ("data:image/png;base64," + event.data.data);
+    output.hidden = false;
+};
+
+worker.onerror = workerOnError;
+worker.onmessage = workerOnMessage;
+
+randomFill.onclick = (e) => {
+    const random = (length = 8) => {
+        // Declare all characters
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        // Pick characers randomly
+        let str = '';
+        for (let i = 0; i < length; i++) {
+            str += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        return str;
+
+    };
+    seed.value = random();
+
+};
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    main();
+}
+
+form.oninput = (e) => {
+    main();
+};
+
+function generate(seed, width, height, max_depth, sep_width, min_middle, max_middle) {
+    if (generating) {
+        toGenerate = { seed: seed, width: width, height: height, max_depth: max_depth, sep_width: sep_width, min_middle: min_middle, max_middle: max_middle };
+    } else {
+        worker.postMessage({ seed: seed, width: width, height: height, max_depth: max_depth, sep_width: sep_width, min_middle: min_middle, max_middle: max_middle });
+        generating = true;
+    }
+
+}
+
+function main() {
+    if (!form.checkValidity()) {
+        return;
+    }
+    if (erroring) {
+        setTimeout(main, 10);
+    }
+    submitButton.disabled = true;
+    submitButton.value = "Génération en cours...";
+    generate(Uint8Array.from(document.getElementById("seed").value), Number.parseInt(document.getElementById("width").value), Number.parseInt(document.getElementById("height").value), Number.parseInt(document.getElementById("max-depth").value), Number.parseInt(document.getElementById("sep-width").value), Number.parseInt(document.getElementById("min-middle").value), Number.parseInt(document.getElementById("max-middle").value));
+}
+
+main()
