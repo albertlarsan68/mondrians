a2892749e0075f7dfc67dd23bdef923904b5ef56
Albert Larsan <74931857+albertlarsan68@users.noreply.github.com>
2023-02-07 15:42:15 +0000

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,7 @@ mkdir deploy/$HASH
 cp pkg/mondrians.js pkg/mondrians_bg.wasm deploy/$HASH
 cp static/* deploy/$HASH
 mv deploy/$HASH/index.html deploy/
-# Replace {hash} in index.html with the hash of the current commit
-sed -i "s/{hash}/$HASH/g" deploy/*
+# Replace {hash} in index.html and the artifact files with the hash of the current commit
+sed -i "s/{hash}/$HASH/g" deploy/index.html
+sed -i "s/{hash}/$HASH/g" deploy/$HASH/*.js
+sed -i "s/{hash}/$HASH/g" deploy/$HASH/*.css
